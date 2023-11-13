c9770d0474119bbcdc59de88501faeebd34ce34b
Patrick <patrick.lee.wang@utexas.edu>
2023-11-13 16:40:47 -0500

    added bash script for pharo launcher

--- project/graph/setup-graph.sh
+++ project/graph/setup-graph.sh
@@ -1,3 +1,13 @@
-curl -o pharo-launcher.zip -L https://files.pharo.org/pharo-launcher/linux64
-unzip pharo-launcher.zip
-./pharolauncher/pharo-launcher
+
+# if [ ! -d "/opt/pharo" ]; then
+#     echo "making directory /opt/pharo"
+#     mkdir -p /opt/pharo
+# fi
+
+if [ ! -d "pharolauncher" ]; then
+    echo "downloading pharo launcher"
+    curl -o PharoLauncher.zip -L https://files.pharo.org/pharo-launcher/linux64
+    unzip PharoLauncher.zip
+    rm PharoLauncher.zip 
+fi
+pharolauncher/pharo-launcher 
